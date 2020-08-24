f7993d1681953022739fe876a235cce0c36a7fb7
Hiromasa Iwasaki <hiromasa.iwasaki@gmail.com>
2020-08-24 12:12:20 +0900

    1st commit
    1st commit

---
+++ Assets/SpriteTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteTest : MonoBehaviour
+{
+    float sumtime=0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sumtime+=Time.deltaTime;
+        transform.position = new Vector3(0f,0f,-6f+Mathf.PingPong(sumtime,7f));
+    }
+}
